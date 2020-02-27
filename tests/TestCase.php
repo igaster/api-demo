63cec2e8afe9833a4b1cf8b2e7eede53e0ea6698
Giannis Gasteratos <igasteratos@gmail.com>
2020-02-27 12:31:23 +0200

    Prepare tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,52 @@
 
 namespace Tests;
 
+use Illuminate\Database\SQLiteConnection;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\DB;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use RefreshDatabase;
+
+    // -----------------------------------------------
+    //  Global Setup (Run once)
+    // -----------------------------------------------
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+        // ...
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        // ...
+        parent::tearDownAfterClass();
+    }
+
+
+    // -----------------------------------------------
+    //  Test Setup (Runs before each test)
+    // -----------------------------------------------
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        if (DB::connection() instanceof SQLiteConnection) {
+            DB::statement(DB::raw('PRAGMA foreign_keys=on'));
+        }
+
+        // $this->seed();
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
 }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,17 +2,35 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Subscriber;
+use App\Models\User;
+use Illuminate\Database\SQLiteConnection;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+         $this->seed();
+    }
+
     public function testBasicTest()
     {
         $this->assertTrue(true);
+    }
+
+    public function testDatabaseIsSeeded()
+    {
+        $this->assertEquals(5, Subscriber::count());
+
+        $this->seed();
+
+        $this->assertEquals(10, Subscriber::count());
     }
 }
